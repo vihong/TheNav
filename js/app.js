8830874520009de50d963f474933bbf0fb0a4533
vihong <probamaths@gmail.com>
2019-09-22 17:07:50 +0200

    first commit pour debug

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    // console.log(navLinks);
+
+    //Toggle Nav
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+        // nav.classList.toggle('nav-active2');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+                // console.log(index / 7);
+            }
+        });
+
+        //Burger rotation
+        burger.classList.toggle('toggle');
+    });
+
+
+
+};
+
+navSlide();
